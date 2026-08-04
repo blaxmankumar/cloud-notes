a6c4f36dafb561563f2519013d58607d94a1eb16
Battula Laxman kumar <99953615+blaxmankumar@users.noreply.github.com>
2026-08-04 16:00:27 +0530

    Delete terraform directory

--- terraform/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "cloud-notes-tfstate-038832652205"
-    key          = "azure-devsecops/dev/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
